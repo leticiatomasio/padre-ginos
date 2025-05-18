dc603a2f0637bf974aea13727c4d29f93e667df1
Leticia Soares Tomasio <leticia.tomasio@totvs.com.br>
2025-05-18 08:38:35 -0300

    config tanstack lazy loading

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,13 +8,13 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:3000",
-        changeOrigin: true
+        changeOrigin: true,
       },
       "/public": {
         target: "http://localhost:3000",
-        changeOrigin: true
-      }
-    }
+        changeOrigin: true,
+      },
+    },
   },
-  plugins: [react()],
-})
+  plugins: [TanStackRouterVite(), react()],
+});
